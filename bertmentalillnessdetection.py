0c88bdd0f33cf05cf75b6becf38154facff6e0c9
NinSean <56484705+NinSean@users.noreply.github.com>
2024-02-27 10:23:33 -0500

    Update bertmentalillnessdetection.py

--- bertmentalillnessdetection.py
+++ bertmentalillnessdetection.py
@@ -9,19 +9,24 @@ Original file is located at
 
 !pip install -q transformers datasets
 
+
 from datasets import load_dataset
 
+
 dataset = load_dataset("sem_eval_2018_task_1", "subtask5.english")
+
 
 dataset
 
 example = dataset['train'][0]
 example
 
+
 labels = [label for label in dataset['train'].features.keys() if label not in ['ID', 'Tweet']]
 id2label = {idx:label for idx, label in enumerate(labels)}
 label2id = {label:idx for idx, label in enumerate(labels)}
 labels
+
 
 from transformers import AutoTokenizer
 import numpy as np
@@ -48,18 +53,28 @@ def preprocess_data(examples):
 
 encoded_dataset = dataset.map(preprocess_data, batched=True, remove_columns=dataset['train'].column_names)
 
+
 example = encoded_dataset['train'][0]
 print(example.keys())
 
+
 tokenizer.decode(example['input_ids'])
+
 
 example['labels']
 
+
 [id2label[idx] for idx, label in enumerate(example['labels']) if label == 1.0]
+
+
 
 encoded_dataset.set_format("torch")
 
+
+
 from transformers import AutoModelForSequenceClassification
+
+
 
 model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased",
                                                            problem_type="multi_label_classification",
@@ -67,10 +82,16 @@ model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased",
                                                            id2label=id2label,
                                                            label2id=label2id)
 
+
+
 batch_size = 8
 metric_name = "f1"
 
+
+
 from transformers import TrainingArguments, Trainer
+
+
 
 args = TrainingArguments(
     f"bert-finetuned-sem_eval-english",
@@ -86,9 +107,13 @@ args = TrainingArguments(
     #push_to_hub=True,
 )
 
+
+
 from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
 from transformers import EvalPrediction
 import torch
+
+
 
 def multi_label_metrics(predictions, labels, threshold=0.5):
     # first, apply sigmoid on predictions which are of shape (batch_size, num_labels)
@@ -108,6 +133,8 @@ def multi_label_metrics(predictions, labels, threshold=0.5):
                'accuracy': accuracy}
     return metrics
 
+
+
 def compute_metrics(p: EvalPrediction):
     preds = p.predictions[0] if isinstance(p.predictions,
             tuple) else p.predictions
@@ -116,13 +143,21 @@ def compute_metrics(p: EvalPrediction):
         labels=p.label_ids)
     return result
 
+
+
 encoded_dataset['train'][0]['labels'].type()
 
+
+
 encoded_dataset['train']['input_ids'][0]
+
+
 
 #forward pass
 outputs = model(input_ids=encoded_dataset['train']['input_ids'][0].unsqueeze(0), labels=encoded_dataset['train'][0]['labels'].unsqueeze(0))
 outputs
+
+
 
 trainer = Trainer(
     model,
@@ -133,19 +168,33 @@ trainer = Trainer(
     compute_metrics=compute_metrics
 )
 
+
+
 trainer.train()
+
+
 
 trainer.evaluate()
 
+
+
 text = "free from this fucking hell"
+
+
 
 encoding = tokenizer(text, return_tensors="pt")
 encoding = {k: v.to(trainer.model.device) for k,v in encoding.items()}
 
+
+
 outputs = trainer.model(**encoding)
+
+
 
 logits = outputs.logits
 logits.shape
+
+
 
 #apply sigmoid + threshold
 sigmoid = torch.nn.Sigmoid()
